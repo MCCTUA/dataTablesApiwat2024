8f882dd7553e77c021f4586bd1d3c0b5a1a317a2
MCCTUA <mcctua@gmail.com>
2024-01-14 22:45:50 +0700

    update & show data on modal แยกระหว่าง เพิ่มข้อมูลใหม่ กับ update ข้อมูล

--- src/code.js
+++ src/code.js
@@ -1,61 +1,84 @@
 function doGet() {
-    return HtmlService.createTemplateFromFile('index')
-        .evaluate()
-        .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+  return HtmlService.createTemplateFromFile('index')
+    .evaluate()
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
 }
 
 //----------------------------------------------------------------------
 function include(file) {
-    return HtmlService.createHtmlOutputFromFile(file).getContent()
+  return HtmlService.createHtmlOutputFromFile(file).getContent()
 }
 //----------------------------------------------------------------------
 
 function getDataWorkSheet() {
-    let ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet1')
-    let data = ss.getDataRange().getDisplayValues()
+  let ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet1')
+  let data = ss.getDataRange().getDisplayValues()
 
-    let dataObj = {
-        ss,
-        data
-    }
+  let dataObj = {
+    ss,
+    data,
+  }
 
-    return dataObj
+  return dataObj
 }
 
 function getData() {
-    let { data } = getDataWorkSheet()
-    let headers = data.shift()
-    return { data: data, headers: headers }
+  let { data } = getDataWorkSheet()
+  let headers = data.shift()
+  return { data: data, headers: headers }
 }
 
 function saveData(obj) {
-    const ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1')
-    const folder = DriveApp.getFolderById('1hjgAMvEB2t5h3B4e5T2nSgVdbZUdbAp6')
-    if (obj.myfile) {
-        const fileUrl = folder.createFile(obj.myfile).getUrl()
-        const fileId = fileUrl.split('/')[5]
-        ss.appendRow([
-            obj.input1,
-            obj.input2,
-            "'" + obj.input3,
-            obj.input4,
-            obj.input5,
-            obj.input6,
-            `https://lh3.googleUserContent.com/d/${fileId}`
-        ])
-    } else {
-        ss.appendRow([obj.input1, obj.input2, "'" + obj.input3, obj.input4, obj.input5, obj.input6])
-    }
+  const ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1')
+  const folder = DriveApp.getFolderById('1hjgAMvEB2t5h3B4e5T2nSgVdbZUdbAp6')
+  if (obj.myfile) {
+    const fileUrl = folder.createFile(obj.myfile).getUrl()
+    const fileId = fileUrl.split('/')[5]
+    ss.appendRow([
+      obj.input1,
+      obj.input2,
+      "'" + obj.input3,
+      obj.input4,
+      obj.input5,
+      obj.input6,
+      `https://lh3.googleUserContent.com/d/${fileId}`,
+    ])
+  } else {
+    ss.appendRow([
+      obj.input1,
+      obj.input2,
+      "'" + obj.input3,
+      obj.input4,
+      obj.input5,
+      obj.input6,
+    ])
+  }
+}
+
+function updateData(obj) {
+  const { data, ss } = getDataWorkSheet()
+  let idRow = data.map((row) => row[0])
+  let index = idRow.indexOf(obj.rowId)
+  console.log(index)
+  console.log(idRow)
+  let updateRow = [
+    obj.input1,
+    obj.input2,
+    "'" + obj.input3,
+    obj.input4,
+    obj.input5,
+    obj.input6,
+  ]
+  ss.getRange(index + 1, 1, 1, 6).setValues([updateRow])
 }
 
 function deleteRecord(numId) {
-    const { data, ss } = getDataWorkSheet()
-    let idRow = data.map(row => row[0])
-    let index = idRow.indexOf(numId)
-    let file = ss.getRange(index + 1, 7).getValue()
-    let idFile = file.split('/')[4]
-    DriveApp.getFileById(idFile).setTrashed(true)
-    ss.deleteRow(index + 1)
-
+  const { data, ss } = getDataWorkSheet()
+  let idRow = data.map((row) => row[0])
+  let index = idRow.indexOf(numId)
+  let file = ss.getRange(index + 1, 7).getValue()
+  let idFile = file.split('/')[4]
+  DriveApp.getFileById(idFile).setTrashed(true)
+  ss.deleteRow(index + 1)
 }
