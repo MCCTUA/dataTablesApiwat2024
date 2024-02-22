545b640cb9019e1f4f79a68562b0e8b47477d0bf
MCCTUA <mcctua@gmail.com>
2024-02-22 22:48:11 +0700

    EP32 Datatable 2024 การแสดงข้อมูลแบบ Popup ด้วย Sweet Alert

--- src/code.js
+++ src/code.js
@@ -38,34 +38,40 @@ function getData() {
 }
 
 function saveData(obj) {
-  const ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1')
-  const folder = DriveApp.getFolderById('1hjgAMvEB2t5h3B4e5T2nSgVdbZUdbAp6')
-  if (obj.myfile) {
-    const fileUrl = folder.createFile(obj.myfile).getUrl()
-    const fileId = fileUrl.split('/')[5]
-    let idRec = idAuto.getID(ss)
-    ss.appendRow([
-      idRec,
-      obj.input2,
-      "'" + obj.input3,
-      obj.input4,
-      obj.input5,
-      new Date(obj.input6).toLocaleDateString('th-TH', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-      }),
-      `https://lh3.googleUserContent.com/d/${fileId}`,
-    ])
-  } else {
-    ss.appendRow([
-      obj.input1,
-      obj.input2,
-      "'" + obj.input3,
-      obj.input4,
-      obj.input5,
-      obj.input6,
-    ])
+  try {
+    const ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1')
+    const folder = DriveApp.getFolderById('1hjgAMvEB2t5h3B4e5T2nSgVdbZUdbAp6')
+    if (obj.myfile) {
+      const fileUrl = folder.createFile(obj.myfile).getUrl()
+      const fileId = fileUrl.split('/')[5]
+      let idRec = idAuto.getID(ss)
+      ss.appendRow([
+        idRec,
+        obj.input2,
+        "'" + obj.input3,
+        obj.input4,
+        obj.input5,
+        obj.input6,
+        new Date(obj.input7).toLocaleDateString('th-TH', {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+        }),
+        `https://lh3.googleUserContent.com/d/${fileId}`,
+      ])
+    } else {
+      ss.appendRow([
+        obj.input1,
+        obj.input2,
+        "'" + obj.input3,
+        obj.input4,
+        obj.input5,
+        obj.input6,
+        obj.input7,
+      ])
+    }
+  } catch (error) {
+    console.error('Error', error)
   }
 }
 
@@ -74,7 +80,10 @@ function updateData(obj) {
     const { ss, idRow, folder } = getDataWorkSheet()
     // rowId มาจาก <input id="rowId" name="rowId" hidden/>
     let index = idRow.indexOf(obj.rowId)
-    let oldLink = ss.getRange(index + 1, 7).getValue()
+    console.log('index', idRow, index)
+    let oldLink = ss.getRange(index + 1, 8).getValue()
+    console.log('old link', oldLink)
+    console.log(' get range : ', ss.getRange(index + 1, 2, 1, 7).getValues)
     let saveLink = oldLink
     // เช็คว่ามี file ถูก upload มาหรือไม่
     if (obj.myfile.length > 0) {
@@ -91,10 +100,13 @@ function updateData(obj) {
       obj.input4,
       obj.input5,
       obj.input6,
+      obj.input7,
       saveLink,
     ]
+    console.log('update row : ', updateRow)
     // id เราไม่แก้ไขดังนี้น ต้องเร่ิมตั้งแต่ column 2 เป็นต้นไป
-    ss.getRange(index + 1, 2, 1, 6).setValues([updateRow])
+
+    ss.getRange(index + 1, 2, 1, 7).setValues([updateRow])
   } catch (error) {
     console.error('Error', error)
   }
